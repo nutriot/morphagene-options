67e3629c30e282c338c203f6461de7959efcf6ae
Jan T. Sott <git@idleberg.com>
2023-11-08 00:49:02 +0100

    update schema

--- src/schema.ts
+++ src/schema.ts
@@ -4,26 +4,26 @@ const roundNumber = new Intl.NumberFormat('en-US', {
 	minimumFractionDigits: 0, maximumFractionDigits: 5
 });
 
-const RANGE_0_TO_1 = z.number().int().min(0).max(1).optional();
-const RANGE_0_TO_2 = z.number().int().min(0).max(2).optional();
+const RANGE_0_TO_1 = z.number().int().min(0).max(1);
+const RANGE_0_TO_2 = z.number().int().min(0).max(2);
 const CHORD_RATIO = z.number().min(-16.000).max(16.000).refine(value => String(value).length <= roundNumber.format(value).length, {
 	message: `Ratios can't have more than five decimals`
 }).optional();
 
 export const schema = z.object({
 	_: z.string(),
-	vsop: RANGE_0_TO_2,
-	inop: RANGE_0_TO_2,
-	pmin: RANGE_0_TO_2,
-	omod: RANGE_0_TO_1,
-	gnsm: RANGE_0_TO_1,
-	rsop: RANGE_0_TO_1,
-	pmod: RANGE_0_TO_2,
-	ckop: RANGE_0_TO_2,
-	cvop: RANGE_0_TO_1,
-	mcr1: CHORD_RATIO,
-	mcr2: CHORD_RATIO,
-	mcr3: CHORD_RATIO,
+	vsop: RANGE_0_TO_2.optional(),
+	inop: RANGE_0_TO_2.optional(),
+	pmin: RANGE_0_TO_2.optional(),
+	omod: RANGE_0_TO_1.optional(),
+	gnsm: RANGE_0_TO_1.optional(),
+	rsop: RANGE_0_TO_1.optional(),
+	pmod: RANGE_0_TO_2.optional(),
+	ckop: RANGE_0_TO_2.optional(),
+	cvop: RANGE_0_TO_1.optional(),
+	mcr1: CHORD_RATIO.optional(),
+	mcr2: CHORD_RATIO.optional(),
+	mcr3: CHORD_RATIO.optional(),
 });
 
 export const validate = (inputSchema) => {
