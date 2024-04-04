9eb0c718086cc9cf3e6c3c7e8d77f4567338c9f1
Jan T. Sott <git@idleberg.com>
2024-04-04 22:39:35 +0200

    add fs entrypoint

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,7 +4,10 @@ export default defineConfig({
 	target: 'esnext',
   clean: true,
   dts: true,
-  entry: ['src/index.ts'],
+  entry: [
+		'src/index.ts',
+		'src/fs.ts'
+	],
   format: 'esm',
   minify: true,
   treeshake: 'recommended'
